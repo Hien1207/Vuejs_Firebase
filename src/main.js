1cf3f05380fb5ed5e6a5075ca459203eeb4890b5
minhcu <minhdong1831999@gmail.com>
2021-07-26 15:42:42 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ElementUI from 'element-ui';
+import firebase from "firebase/app";
+
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+Vue.use(firebase);
 
 new Vue({
   router,
