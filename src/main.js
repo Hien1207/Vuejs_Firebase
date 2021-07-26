6f5b8fca6158f3ac45b77e43627b5dffff670cdf
minhcu <minhdong1831999@gmail.com>
2021-07-26 16:03:53 +0700

    vuex

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import ElementUI from 'element-ui';
 import firebase from "firebase/app";
-
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 Vue.use(ElementUI);
 
