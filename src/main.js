ec29e324056d4268140021b2a55129e7d3fea90d
minhcu <minhdong1831999@gmail.com>
2021-07-28 10:06:13 +0700

    update firebase

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import ElementUI from 'element-ui';
 import firebase from "firebase/app";
-import Vuex from 'vuex'
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false;
 
@@ -11,7 +11,18 @@ Vue.use(Vuex);
 
 Vue.use(ElementUI);
 
-Vue.use(firebase);
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwlilc25tJEN9RnQG_RerZfUSQdVC9cR8",
+  authDomain: "vuejs-firebase-1d336.firebaseapp.com",
+  projectId: "vuejs-firebase-1d336",
+  storageBucket: "vuejs-firebase-1d336.appspot.com",
+  messagingSenderId: "1049087402909",
+  appId: "1:1049087402909:web:b3139815b2b3b7910e4574",
+  measurementId: "G-6VL25LDRT4"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
