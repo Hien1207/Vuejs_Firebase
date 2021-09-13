ab48dafc2f56014d0b6618e65e4933c7688fc1d4
Hien1207 <78089627+Hien1207@users.noreply.github.com>
2021-09-14 00:31:21 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -78,7 +78,7 @@ export default {
    },
  methods:{
    gotoUserDetailPage(id){
-     this.$router.push(`/user/${id}`)
+     this.$router.push(`/blogs/${id}`)
    }  
  },
 }
@@ -141,7 +141,7 @@ export default {
 }
 .imga{
   width:100%;
-  height:100px;
+  height:100%;
   cursor: pointer;
 }
 .VueStar {
@@ -230,7 +230,7 @@ export default {
   }
   .imga{
   width:100%;
-  height:260px;
+  height:100%;
   }
   .VueStar__ground{
     margin-left: 12.5rem;
